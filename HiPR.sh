e83eeb3fe50d69c95a5d282ca92013802a483535
Pavel Kuksa <pkuksa@sciget.pmacs.upenn.edu>
2017-06-28 20:10:02 -0400

    updated HiPR.sh

--- HiPR.sh
+++ HiPR.sh
@@ -1,5 +1,10 @@
+# functions
+function die {
+   echo "$1" >&2
+   exit 1
+}
 
-# check HiPR
+# check HiPR executables
 scriptDir=`dirname $0`
 PATH=$PATH:${scriptDir}
 mcmcCmd=HiPR_MCMC
@@ -12,35 +17,78 @@ if [ ! -s "${HIPRMCMCwrapper}" ]; then
   exit 1;
 fi
 
-# required inputs
+# required inputs [must be first 3 arguments]
 COLLAPSEDREADFILE=$1 # DMS.collapsed.reads
-STARTSTRUCTFILE=$2 # e.g., structures sampled from Boltzmann distribution
-MODRATEFILE=$3 # per-nucleotide modification rates for unpaired/paired bases
+MODRATEFILE=$2 # per-nucleotide modification rates for unpaired/paired bases
+STARTSTRUCTFILE=$3 # e.g., structures sampled from Boltzmann distribution
 
 if [ $# -lt 3 ]; then
-  echo >&2 "USAGE: $0 reads_file rates_file structure_file"
+  #echo >&2 "USAGE: $0 reads_file rates_file structure_file [OutputDir] [LocusName] [Niter] [Ncpus] [ModelType] [MinReadLength] [MaxReadLength]"
+  echo >&2 "USAGE: $0 reads_file rates_file structure_file [-outDir OutputDir] [-locusName LocusName] [-n Niter] [-numCPU Ncpus]  [-rmin MinReadLength] [-rmax MaxReadLength]"
   exit 1;
 fi
 
+if [ ! -s "${COLLAPSEDREADFILE}" ]; then
+  die "${COLLAPSEDREADFILE} does not exist or is empty!"
+fi
+if [ ! -s "${MODRATEFILE}" ]; then
+  die "${MODRATEFILE} does not exist or is empty!"
+fi
+if [ ! -s "${STARTSTRUCTFILE}" ]; then
+  die "${STARTSTRUCTFILE} does not exist or is empty!"
+fi
+
+# process optional arguments
+while [ -n "$1" ]; do
+	case $1 in
+		-rmin)
+                  rmin=$2
+                  shift
+                  ;;
+                -rmax)
+                  rmax=$2
+                  shift
+                  ;;
+                -numCPU)
+                  numCPU=$2
+                  shift
+                  ;;
+                -n)
+                  n=$2
+                  shift
+                  ;;
+                -outDir)
+                  outDir=$2
+                  shift
+                  ;;
+                -locusName)
+                  locusName=$2
+                  shift
+                  ;;
+                -?*)
+                  echo "Unknown option (ignored): $1" >&2
+                  ;;
+        esac
+
+        shift
+done
+
+
 # output options
-OUTDIR=${4:-HiPR_output} # output folder
-LOCUSNAME=${5:-UnnamedLocus} # locus name
+OUTDIR=${outDir:-HiPR_output} # output folder
+LOCUSNAME=${locusName:-UnnamedLocus} # locus name
 
 # MCMC run parameters
-NITER=${6:-100000}
-NCPU=${7:-16}
-MODELTYPE=${8:-2} # set to 2
-MINREADLEN=${9:-15}
-MAXREADLEN=${10:-60}
-
+NITER=${n:-100000}
+NCPU=${numCPU:-16}
+MODELTYPE=${modelType:-2} # set to 2
+MINREADLEN=${rmin:-15}
+MAXREADLEN=${rmax:-60}
 
 LOGDIR="${OUTDIR}/logs"
 
-
-#QSUB="bsub"
-#jobName="mcmc_${LOCUSNAME}"
-
-mkdir -p ${OUTDIR} # make output dir if does not exist already
+# make output directories if do not exist already
+mkdir -p "${OUTDIR}"
 mkdir -p "${LOGDIR}"
 
 NCHAIN=$(awk 'END{print NR/2}' ${STARTSTRUCTFILE}) # number of MCMC chains
@@ -48,11 +96,13 @@ head -n 1 ${STARTSTRUCTFILE} > ${OUTDIR}/${LOCUSNAME}.seq # primary sequence
 
 echo "#chains=${NCHAIN}"
 
+# set #CPUs to use
 ncpus=$(cat /proc/cpuinfo | awk '{if ($0~/^processor/) ++ncpu}END{print ncpu+0}') 
-  echo >&2 "#cpus detected=${ncpus}"
-  echo >&2 "#cpus requested=${NCPU}"
+echo >&2 "#cpus detected=${ncpus}"
+echo >&2 "#cpus requested=${NCPU}"
 if [ "${ncpus}" -gt "0" ] && [ "$NCPU" -gt "${ncpus}" ]; then
   echo >&2 "#requesting ${ncpus}"
   NCPU="${ncpus}"
 fi
+
 perl ${HIPRMCMCwrapper} -id ${LOCUSNAME} -rmin ${MINREADLEN} -rmax ${MAXREADLEN} -burn_in 10 -samp 10 -t sd -numCPU ${NCPU} -n ${NITER} -m ${MODELTYPE} -outdir ${OUTDIR} ${COLLAPSEDREADFILE} ${MODRATEFILE} ${STARTSTRUCTFILE} > /dev/null && bash dms_mcmc_collect_structures.sh ${LOCUSNAME} ${OUTDIR} ${NCHAIN} > ${OUTDIR}/${LOCUSNAME}.mcmc.combined.struct && bash dms_get_majority_structure.sh ${OUTDIR}/${LOCUSNAME}.mcmc.combined.struct && cat ${OUTDIR}/${LOCUSNAME}.seq ${OUTDIR}/${LOCUSNAME}.mcmc.combined.struct.majority_consensus > ${OUTDIR}/${LOCUSNAME}.mcmc.majority_consensus
